f11cacda1ca586ff605d21608dfa9c2e06660462
Jamey Clark Johnson <clarkandkathy@gmail.com>
2019-10-08 15:11:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,14 @@ class Dog
   puts "I'm a dog"
 end
 
+
+class Person
+  puts "I'm a person"
+end
+
 fido = Dog.new 
 snoopy = Dog.new
 lassie = Dog.new
+
+adele_goldberg = Person.new
+alan_kay = Person.new
